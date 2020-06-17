f52376a1d7324275e0971aa2612c871c82fe818a
Denis Medvedev <mr.raz4291@gmail.com>
2020-06-17 14:47:10 +0500

    ts-util

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,109 @@
-import { Hello } from ".";
+import * as Util from ".";
 
-describe("Test", () => {
-    test("Hello", () => {
-        expect(Hello()).toBe("hello world!");
+const match = (s: string) => Util.firstMatch(s, Util.REGEX_FILENAME);
+
+describe("Util (Regex function)", () => {
+    test("REGEX_FILENAME", () => {
+        expect(match("d:/web/js-lib/src/main.ts")).toBe("main.ts");
+        expect(match("d:/web/js-lib/src/main.js")).toBe("main.js");
+        expect(match("./main.ts")).toBe("main.ts");
+        expect(match("./main.json")).toBe("main.json");
+        expect(match("./main.spec.js")).toBe("main.spec.js");
+        expect(match("./main-js")).toBeUndefined();
+    });
+    test("REGEX_COMMAND_LINE", () => {
+        expect("rank Gold 1".match(Util.REGEX_COMMAND_LINE)).toEqual(["rank", "Gold", "1"]);
+        expect("rank \"Gold 1\"".match(Util.REGEX_COMMAND_LINE)).toEqual(["rank", "\"Gold 1\""]);
+    });
+    test("REGEX_QUOTES", () => {
+        expect("\"Gold 1\"".replace(Util.REGEX_QUOTES, "&quot;")).toBe("&quot;Gold 1&quot;");
+        expect("'Gold 1'".replace(Util.REGEX_QUOTES, "&quot;")).toBe("&quot;Gold 1&quot;");
+        expect("' 'Gold 1' '".replace(Util.REGEX_QUOTES, "&quot;")).toBe("&quot; 'Gold 1' &quot;");
+    });
+    test("REGEX_COMMAND_LINE", () => {
+        expect("rank Gold 1".match(Util.REGEX_COMMAND_LINE)).toEqual(["rank", "Gold", "1"]);
+        expect("rank \"Gold 1\"".match(Util.REGEX_COMMAND_LINE)).toEqual(["rank", "\"Gold 1\""]);
+    });
+    test("REGEX_BASE64", () => {
+        expect(Util.REGEX_BASE64.test("data:text/plain;base64,aGk=")).toBeTruthy();
+    });
+});
+
+describe("Util (Pure function)", () => {
+    test("commandArgs", () => {
+        expect(Util.commandArgs("!kick raziEiL")).toEqual(["!kick", "raziEiL"]);
+        expect(Util.commandArgs("!kick raziEiL \"coding too much!\"")).toEqual(["!kick", "raziEiL", "coding too much!"]);
+        expect(Util.commandArgs("!kick raziEiL 'coding too much!'")).toEqual(["!kick", "raziEiL", "coding too much!"]);
+    });
+    test("firstMatch", () => {
+        expect(Util.firstMatch("1 2 3 4 5", /\d/g)).toBe("1");
+        expect(Util.firstMatch("hi 1 2", /\d/g)).toBe("1");
+        expect(Util.firstMatch("hi 1 2", /\d/)).toBe("1");
+        expect(Util.firstMatch("hi", /\d/)).toBeFalsy();
+    });
+    test("moduleName", () => {
+        expect(Util.moduleName(module)).toBe("index.spec.ts");
+    });
+    test("moduleNameEx", () => {
+        expect(Util.moduleNameEx()).toBe("index.spec.ts");
+    });
+    test("isPrimitive", () => {
+        expect(Util.isPrimitive(2020)).toBeTruthy();
+        expect(Util.isPrimitive(3.14)).toBeTruthy();
+        expect(Util.isPrimitive(true)).toBeTruthy();
+        expect(Util.isPrimitive("hi")).toBeTruthy();
+        expect(Util.isPrimitive(Symbol("sym"))).toBeTruthy();
+        expect(Util.isPrimitive(Number.NaN)).toBeTruthy();
+        expect(Util.isPrimitive(null)).toBeTruthy();
+        expect(Util.isPrimitive(undefined)).toBeTruthy();
+        expect(Util.isPrimitive([])).toBeFalsy();
+        expect(Util.isPrimitive({ object: true })).toBeFalsy();
+        expect(Util.isPrimitive(() => "this is a function")).toBeFalsy();
+    });
+    test("caclms", () => {
+        expect(Util.caclms(1)).toBe(3600000);
+        expect(Util.caclms(0, 1)).toBe(60000);
+        expect(Util.caclms(0, 0, 1)).toBe(1000);
+        expect(Util.caclms(1, 1, 1)).toBe(3661000);
+    });
+    test("timestamp", () => {
+        expect(Util.timestamp().toString().length).toBe(10);
+    });
+    test("mod", () => {
+        expect(Util.mod(4, 2)).toBe(0);
+        expect(Util.mod(4, 3)).toBe(1);
+        expect(Util.mod(0, 3)).toBe(0);
+        expect(Util.mod(1, 3)).toBe(1);
+        expect(Util.mod(3, 3)).toBe(0);
+        expect(Util.mod(-1, 3)).toBe(2);
+        expect(Util.mod(-4, 2)).toBe(0);
+        // expect(Util.mod(4, -2)).toBe(-0); // -0 wtf ???
+    });
+    test("precentWhatLeft", () => {
+        expect(Util.precentWhatLeft(0, 1000)).toBe(100);
+        expect(Util.precentWhatLeft(100, 1000)).toBe(90);
+    });
+    test("precentWhat", () => {
+        expect(Util.precentWhat(0, 1000)).toBe(0);
+        expect(Util.precentWhat(100, 1000)).toBe(10);
+    });
+    test("renameKey", () => {
+        const obj = { old: "Hi" };
+        Util.renameKey(obj, "old", "new");
+        expect(obj).toEqual({ new: "Hi" });
+    });
+    test("factorial", () => {
+        expect(Util.factorial(0)).toBe(1);
+        expect(Util.factorial(5)).toBe(120);
+    });
+    test.skip("time (should be always √)", () => { });
+});
+
+describe("Util (Formatted function)", () => {
+    test("f_moduleTag", () => {
+        expect(Util.f_moduleName(module)).toBe("[index.spec.ts]");
+    });
+    test("f_precentWhatLeft", () => {
+        expect(Util.f_precentWhatLeft(0, 1000)).toBe("100%");
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-export function Hello() {
-    return "hello world!";
-}
-
-console.log(Hello());
+// Regular expression
+export const REGEX_FILENAME = /[^/\\]+\.(.+)$/;
+export const REGEX_COMMAND_LINE = /".+"|'.+'|\S+/g;
+export const REGEX_QUOTES = /^"|"$|^'|'$/g; // detects only start and end quotes
+export const REGEX_BASE64 = /^data:([+/A-Za-z-]+);base64,(.+)$/;
+// Pure function
+export const commandArgs = (s: string) => { const m = s.match(REGEX_COMMAND_LINE); if (m) return m.map(s => s.replace(REGEX_QUOTES, "")); };
+export const firstMatch = (s: string, regex: RegExp) => { const r = s.match(regex); return r ? r[0] : undefined };
+export const moduleName = (m: NodeModule): string => firstMatch(m.filename, REGEX_FILENAME) || "NULL!";
+export const moduleNameEx = () => module.parent ? (firstMatch(module.parent.filename, REGEX_FILENAME) || "NULL!") : "NULL!";
+export const isPrimitive = (val: any): boolean => val === null || (typeof val !== "object" && typeof val !== "function");
+export const caclms = (h: number, m = 0, s = 0) => (h * 3600 + m * 60 + s) * 1000;
+export const timestamp = () => Math.floor(Date.now() / 1000); // Unix time in seconds
+export const mod = (x: number, n: number) => (x % n + n) % n;
+export const precentWhatLeft = (current: number, max: number): number => Math.floor((max - current) / max * 100);
+export const precentWhat = (current: number, max: number): number => Math.floor(current * 100 / max);
+export const sleep = (ms: number) => new Promise(res => { setTimeout(res, ms) });
+export const renameKey = (obj: any, oldKey: any, newKey: any) => { delete Object.assign(obj, { [newKey]: obj[oldKey] })[oldKey] };
+export const factorial = (n: number): number => n <= 1 ? 1 : n * factorial(n - 1); // 5! = 1*2*3*4*5=120
+export const time = () => { const time = new Date(); return `${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}` };
+export const escapeJSON = (s: string) => s.replace(/\\n/g, "\\n").replace(/\\'/g, "\\'").replace(/\\"/g, '\\"').replace(/\\&/g, "\\&").replace(/\\r/g, "\\r").replace(/\\t/g, "\\t").replace(/\\b/g, "\\b").replace(/\\f/g, "\\f");
+// Formatted function
+export const f_moduleName = (m: NodeModule): string => `[${moduleName(m)}]`;
+export const f_precentWhatLeft = (current: number, max: number): string => precentWhatLeft(current, max) + "%";
